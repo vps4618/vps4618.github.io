c781571de40dfd7818f1ae87758a70fc30a8542d
vishwa praveen <vishwapraveen4618@gmail.com>
2022-11-21 01:59:06 +0530

    comment system added

---
+++ hand-cricket/js/comment.js
@@ -0,0 +1,169 @@
+"use strict";
+
+// loads comments when page loads
+// get request
+const xhrGet = new XMLHttpRequest();
+xhrGet.open("GET", "https://handcricketcomments.glitch.me/api/comments");
+xhrGet.responseType = "json";
+xhrGet.onload = () => {
+  if (
+    xhrGet.readyState == 4 &&
+    (xhrGet.status == 200 || xhrGet.status == 201)
+  ) {
+    const commentsArray = xhrGet.response.records;
+    commentsArray.map((comment) => {
+      commentsSection.innerHTML += `<div class="item">
+                                            <p class="id">${comment.id}</p>
+                                            <p class="name">${
+                                              comment.firstName +
+                                              " " +
+                                              comment.lastName
+                                            } <small>${comment.dateTime}</small>
+                                            </p>
+                                            <p  class="comment">${
+                                              comment.comment
+                                            }</p>
+                                        </div>`;
+    });
+  } else {
+    const data = xhrGet.response;
+    const error = JSON.parse(data);
+    commentsSection.innerHTML = `<div class="item">
+                                            <p class="id">${error}</p>
+                                            <p class="name">${error}</p>
+                                            <p  class="comment">${error}</p>
+                                        </div>`;
+  }
+};
+
+xhrGet.send();
+
+const form = document.getElementById("form");
+const commentButton = document.getElementById("commentButton");
+const message = document.getElementById("message");
+const commentsSection = document.getElementById("commentsSection");
+
+function showForm() {
+  form.style.display = "block";
+  commentButton.style.display = "none";
+}
+
+function submitComment() {
+  let firstName = document.getElementById("firstNameInput").value;
+  let lastName = document.getElementById("lastNameInput").value;
+  let comment = document.getElementById("commentInput").value;
+  let today = new Date();
+  let fullYear = today.getFullYear().toString();
+  let month = today.getMonth().toString();
+  let day = today.getDate().toString();
+  let hour = today.getHours().toString();
+  let minutes = today.getMinutes().toString();
+
+  let dateTime =
+    fullYear + "-" + month + "-" + day + " > " + hour + " : " + minutes;
+
+  if (firstName === "") {
+    message.innerHTML = "firstName field empty !";
+    message.className = "error";
+    return;
+  } else if (lastName === "") {
+    message.innerHTML = "lastName field empty !";
+    message.className = "error";
+    return;
+  } else if (comment === "") {
+    message.innerHTML = "comment field empty !";
+    message.className = "error";
+    return;
+  } else {
+    try {
+      // post comment
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://handcricketcomments.glitch.me/api/comments");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      const body = JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        comment: comment,
+        dateTime: dateTime,
+      });
+      xhr.onload = () => {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 201)) {
+          message.innerHTML = JSON.parse(xhr.responseText).message;
+          message.className = "info";
+
+          setTimeout(() => {
+            // get request
+            const xhrGet = new XMLHttpRequest();
+            xhrGet.open(
+              "GET",
+              "https://handcricketcomments.glitch.me/api/comments"
+            );
+            xhrGet.responseType = "json";
+            xhrGet.onload = () => {
+              if (
+                xhrGet.readyState == 4 &&
+                (xhrGet.status == 200 || xhrGet.status == 201)
+              ) {
+                const commentsArray = xhrGet.response.records;
+                commentsSection.innerHTML = "";
+                commentsArray.map((comment) => {
+                  commentsSection.innerHTML += `<div class="item">
+                                                    <p class="id">${
+                                                      comment.id
+                                                    }</p>
+                                                    <p class="name">${
+                                                      comment.firstName +
+                                                      " " +
+                                                      comment.lastName
+                                                    } <small>${
+                    comment.dateTime
+                  }</small></p>
+                                                    <p  class="comment">${
+                                                      comment.comment
+                                                    }</p>
+                                                </div>`;
+                });
+              } else {
+                const data = xhrGet.response;
+                const error = JSON.parse(data);
+                commentsSection.innerHTML = `<div class="item">
+                                                    <p class="id">${error}</p>
+                                                    <p class="name">${error}</p>
+                                                    <p  class="comment">${error}</p>
+                                                </div>`;
+              }
+            };
+
+            xhrGet.send();
+
+            message.innerHTML = "";
+            form.style.display = "none";
+            commentButton.style.display = "block";
+          }, 2000);
+        } else {
+          message.innerHTML = JSON.parse(xhr.responseText).error;
+          message.className = "error";
+
+          setTimeout(() => {
+            message.innerHTML = "";
+            form.style.display = "none";
+            commentButton.style.display = "block";
+          }, 2000);
+        }
+      };
+      xhr.send(body);
+    } catch (err) {
+      message.innerHTML = "error";
+      message.className = "error";
+
+      setTimeout(() => {
+        message.innerHTML = "";
+        form.style.display = "none";
+        commentButton.style.display = "block";
+      }, 2000);
+    }
+    document.getElementById("firstNameInput").value = "";
+    document.getElementById("lastNameInput").value = "";
+    document.getElementById("commentInput").value = "";
+  }
+}
